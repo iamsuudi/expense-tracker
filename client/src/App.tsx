bcf6e5d05aac5e517f94e251378ac698695049a7
Abdulfetah <suudiabdulfetah@gmail.com>
2025-05-20 00:59:03 +0300

    add react with shadcn and tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+      <div className=''>
+        <Button>Click Me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
